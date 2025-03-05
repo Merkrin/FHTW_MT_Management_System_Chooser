7412acea2ae5cdf2a8a02ae1612b6f9169f8f230
Irina Barciuc. <iabarchuk@edu.hse.ru>
2025-03-05 14:18:07 +0100

    Initial commit

---
+++ src/main/java/at/fhtw/mt/management_system_chooser/ManagementSystemChooserApplication.java
@@ -0,0 +1,13 @@
+package at.fhtw.mt.management_system_chooser;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ManagementSystemChooserApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ManagementSystemChooserApplication.class, args);
+    }
+
+}
---
+++ src/test/java/at/fhtw/mt/management_system_chooser/ManagementSystemChooserApplicationTests.java
@@ -0,0 +1,13 @@
+package at.fhtw.mt.management_system_chooser;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ManagementSystemChooserApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
