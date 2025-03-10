d59900fd760416d311377d0fb73df05dc9ee0ed8
Irina Barciuc. <iabarchuk@edu.hse.ru>
2025-03-10 18:32:56 +0100

    WIP commit: 2nd page not working

--- src/main/java/at/fhtw/mt/management_system_chooser/server/controller/QuestionsController.java
+++ src/main/java/at/fhtw/mt/management_system_chooser/server/controller/QuestionsController.java
@@ -1,10 +1,12 @@
 package at.fhtw.mt.management_system_chooser.server.controller;
 
 import at.fhtw.mt.management_system_chooser.database.model.Behavior;
+import at.fhtw.mt.management_system_chooser.database.model.HumanComponent;
 import at.fhtw.mt.management_system_chooser.database.model.QuestionAnswer;
 import at.fhtw.mt.management_system_chooser.database.model.RequirementQuestion;
 import at.fhtw.mt.management_system_chooser.database.repository.QuestionAnswerRepository;
 import at.fhtw.mt.management_system_chooser.database.repository.RequirementQuestionRepository;
+import at.fhtw.mt.management_system_chooser.server.service.QuestionsMapperService;
 import at.fhtw.mt.management_system_chooser.server.utils.CollectionsUtils;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
@@ -23,9 +25,12 @@ public class QuestionsController {
     protected static final Logger logger = LogManager.getLogger();
 
     private final RequirementQuestionRepository requirementQuestionRepository;
+    private final QuestionsMapperService questionsMapperService;
 
-    public QuestionsController(RequirementQuestionRepository requirementQuestionRepository) {
+    public QuestionsController(RequirementQuestionRepository requirementQuestionRepository,
+                               QuestionsMapperService questionsMapperService) {
         this.requirementQuestionRepository = requirementQuestionRepository;
+        this.questionsMapperService = questionsMapperService;
     }
 
     @GetMapping("/")
@@ -42,13 +47,15 @@ public class QuestionsController {
         List<RequirementQuestion> requirementQuestions = requirementQuestionRepository.findAll();
         logger.debug("Loaded {} questions for display", requirementQuestions.size());
 
-        Map<String, List<QuestionAnswer>> questionAnswers = requirementQuestions.stream().collect(
-                Collectors.toMap(RequirementQuestion::getRequirementId, RequirementQuestion::getQuestionAnswers));
+        Map<HumanComponent, Map<Behavior, List<RequirementQuestion>>> questionsByComponentsAndBehaviors = questionsMapperService.getQuestionsByComponentsAndBehaviors(
+                requirementQuestions);
+
+        Map<String, List<QuestionAnswer>> questionAnswers = questionsMapperService.getQuestionAnswers(requirementQuestions);
         logger.debug("Loaded {} answer lists for display, {} answers in total",
                      CollectionsUtils.getMapLength(questionAnswers), CollectionsUtils.getMapSize(questionAnswers));
 
         model.addAttribute("systems", systems);
-        model.addAttribute("questions", requirementQuestions);
+        model.addAttribute("questionsMap", questionsByComponentsAndBehaviors);
         model.addAttribute("answersMap", questionAnswers);
 
         return "requirements_questions";
