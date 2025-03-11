4ee9df38c420af86e4bdc95b62470bb60fc4e866
Irina Barciuc. <iabarchuk@edu.hse.ru>
2025-03-11 11:17:02 +0100

    Created last page

--- src/main/java/at/fhtw/mt/management_system_chooser/server/controller/QuestionsController.java
+++ src/main/java/at/fhtw/mt/management_system_chooser/server/controller/QuestionsController.java
@@ -8,6 +8,7 @@ import at.fhtw.mt.management_system_chooser.database.repository.QuestionAnswerRe
 import at.fhtw.mt.management_system_chooser.database.repository.RequirementQuestionRepository;
 import at.fhtw.mt.management_system_chooser.server.service.QuestionsMapperService;
 import at.fhtw.mt.management_system_chooser.server.utils.CollectionsUtils;
+import jakarta.servlet.http.HttpSession;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.springframework.stereotype.Controller;
@@ -41,8 +42,11 @@ public class QuestionsController {
     }
 
     @PostMapping("/questions")
-    public String getQuestions(@RequestParam List<String> systems, Model model) {
+    public String getQuestions(@RequestParam List<String> systems, Model model, HttpSession session) {
         logger.info("Started questions loading...");
+
+        session.removeAttribute("previousAnswers");
+        session.setAttribute("systems", systems);
 
         List<RequirementQuestion> requirementQuestions = requirementQuestionRepository.findAll();
         logger.debug("Loaded {} questions for display", requirementQuestions.size());
@@ -59,5 +63,39 @@ public class QuestionsController {
         model.addAttribute("answersMap", questionAnswers);
 
         return "requirements_questions";
+    }
+
+    @GetMapping("/questions")
+    public String showQuestions(Model model, HttpSession session) {
+        Map<String, String> previousAnswers = (Map<String, String>) session.getAttribute("previousAnswers");
+        model.addAttribute("previousAnswers", previousAnswers);
+
+        List<RequirementQuestion> requirementQuestions = requirementQuestionRepository.findAll();
+        Map<HumanComponent, Map<Behavior, List<RequirementQuestion>>> questionsByComponentsAndBehaviors = questionsMapperService.getQuestionsByComponentsAndBehaviors(
+                requirementQuestions);
+        Map<String, List<QuestionAnswer>> questionAnswers = questionsMapperService.getQuestionAnswers(requirementQuestions);
+        model.addAttribute("questionsMap", questionsByComponentsAndBehaviors);
+        model.addAttribute("answersMap", questionAnswers);
+
+        model.addAttribute("systems", session.getAttribute("systems"));
+
+        return "requirements_questions";
+    }
+
+    @PostMapping("/submit")
+    public String submitAnswers(@RequestParam Map<String, String> selectedAnswers, Model model, HttpSession session) {
+        session.setAttribute("previousAnswers", selectedAnswers);
+
+
+        Map<String, Long> results = selectedAnswers.entrySet().stream()
+                .filter(entry -> entry.getValue() != null && !entry.getValue().isEmpty())
+                .collect(Collectors.groupingBy(
+                        entry -> entry.getKey().split("_")[0],
+                        Collectors.counting()
+                ));
+
+        model.addAttribute("results", results);
+
+        return "results";
     }
 }
